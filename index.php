5f1a860c64da45cdb877335740bc279144d875ef
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-04 16:27:35 +0200

    fine esercizio base

--- index.php
+++ index.php
@@ -48,16 +48,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <title>Hotel</title>
 </head>
     <body>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="pt-3">
+                            <h1 >
+                                Booladvisor
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+
+        </header>
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-8">
+                <div class=" col-sm 12 col-mg-6 col-lg-8">
                     <?php foreach ($hotels as $key =>$hotel ) { ?>
-                        <table class="table text-center border my-3 ">
+                        <table class="table text-center my-3 border border-4 ">
                             <tbody>
                                 <tr>
                                     <th>
@@ -76,21 +94,21 @@
                                 <tr>
                                     <td>
                                         <div>
-                                            <?php echo   ($hotel['parking'] === true) ? 'Con parcheggio' : 'Senza Parcheggio'; ?>  
+                                            <?php echo  ($hotel['parking'] === true) ? 'Con parcheggio' : 'Senza Parcheggio'; ?>  
                                         </div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <div>
-                                            <?php echo $hotel['vote']  ?>  
+                                           Voto: <?php echo $hotel['vote']  ?>  
                                         </div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
                                         <div>
-                                            <?php echo $hotel['distance_to_center']  ?>  
+                                            <?php echo $hotel['distance_to_center']  ?> km dal centro  
                                         </div> 
                                     </td>
                                 </tr>
