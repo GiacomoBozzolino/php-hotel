c388b4b327ccabc1b525bb31e2f116e8157e9dc5
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-04 15:58:41 +0200

    [FEAT] lavoro stile tabella

--- index.php
+++ index.php
@@ -48,38 +48,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Hotel</title>
 </head>
     <body>
-        <div>
-            <ul>
-                <?php foreach ($hotels as $key =>$hotel ) { ?>
-                <li>
-                
-                    <div>
-                    <?php echo $hotel['name']  ?>  
-                    </div>
-                    <div>
-                    <?php echo $hotel['description']  ?>  
-                    </div>
-                    <div>
-                    <?php echo $hotel['parking']  ?>  
-                    </div>
-                    <div>
-                    <?php echo $hotel['vote']  ?>  
-                    </div>
-                    <div>
-                    <?php echo $hotel['distance_to_center']  ?>  
-                    </div>
-                    
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-8">
+                    <?php foreach ($hotels as $key =>$hotel ) { ?>
+                        <table class="table text-center border my-3 ">
+                            <tbody>
+                                <tr>
+                                    <th>
+                                        <div>
+                                            <?php echo $hotel['name']  ?>  
+                                        </div>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <?php echo $hotel['description']  ?>  
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <?php echo   ($hotel['parking'] === true) ? 'Con parcheggio' : 'Senza Parcheggio'; ?>  
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <?php echo $hotel['vote']  ?>  
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <?php echo $hotel['distance_to_center']  ?>  
+                                        </div> 
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    <?php } ?>
+                </div>
 
-
-                    
-                    
-
-                </li>
-                <?php } ?>
-            </ul>
+            </div>
         </div>
         
     </body>
